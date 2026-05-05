cb2afc45098420438ad75e9bbaf7cc06918169c2
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-05 23:43:41 +0530

    menu for smaller screens added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,14 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <>
-      <nav className="w-full fixed px-4 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 text-ovo">
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] select-none pointer-events-none">
+        <Image
+          src={assets.header_bg_color}
+          alt="background gradient"
+          className="w-full "
+        />
+      </div>
+      <nav className="w-full fixed px-4 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 font-ovo">
         <a href="#top">
           <Image
             src={assets.logo}
@@ -13,9 +20,9 @@ const Navbar = () => {
           />
         </a>
 
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-4">
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-4 bg-white shadow-sm opacity-70">
           <li>
-            <a className="font-ovo" href="#top">Home</a>
+            <a href="#top">Home</a>
           </li>
           <li>
             <a href="#about">About Me</a>
@@ -31,7 +38,11 @@ const Navbar = () => {
           </li>
         </ul>
 
-        <div>
+        <div className="flex items-center gap-4">
+          <button>
+            <Image src={assets.moon_icon} alt="moon" className="w-6" />
+          </button>
+
           <a
             href="#contact"
             className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
@@ -39,7 +50,29 @@ const Navbar = () => {
             Contact{" "}
             <Image src={assets.arrow_icon} alt="arrow" className="w-3" />
           </a>
+
+          <button className="block md:hidden ml-3">
+            <Image src={assets.menu_black} alt="menu" className="w-6" />
+          </button>
         </div>
+
+        <ul className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
+          <li>
+            <a href="#top">Home</a>
+          </li>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#work">My Work</a>
+          </li>
+          <li>
+            <a href="#contact">Contact Me</a>
+          </li>
+        </ul>
       </nav>
     </>
   );
