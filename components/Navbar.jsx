5333c93bcac9bde210250bf019100a1c0735d931
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-07 23:56:28 +0530

    css updated for narbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -32,7 +32,10 @@ const Navbar = () => {
         </ul>
 
         <div>
-          <a href="#contact">
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+          >
             Contact{" "}
             <Image src={assets.arrow_icon} alt="arrow" className="w-3" />
           </a>
