96cc6d9eb9efeecd4b84b46491912a9b0af803e8
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-07 23:45:57 +0530

    logo added to navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,19 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <nav>
+        <a href="">
+          <Image
+            src={assets.logo}
+            alt="Shubham"
+            className="w-28 cursor-pointer mr-14"
+          />
+        </a>
+      </nav>
+    </>
   );
 };
 
