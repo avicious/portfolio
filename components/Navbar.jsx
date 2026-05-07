19a16c16bd8112464f0c51a2e0c05c1074a0bf1c
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-08 00:05:30 +0530

    bg gradient image added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,14 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image
+          src={assets.header_bg_color}
+          alt=""
+          className="w-full select-none pointer-events-none"
+          loading="eager"
+        />
+      </div>
       <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
         <a href="#top">
           <Image
@@ -13,7 +21,7 @@ const Navbar = () => {
           />
         </a>
 
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 font-ovo bg-white/50 shadow-sm">
           <li>
             <a href="#top">Home</a>
           </li>
@@ -34,7 +42,7 @@ const Navbar = () => {
         <div>
           <a
             href="#contact"
-            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo"
           >
             Contact{" "}
             <Image src={assets.arrow_icon} alt="arrow" className="w-3" />
