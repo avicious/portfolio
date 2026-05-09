1718a15d8ca2c9215d02b7b7d61d519c592c10f1
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-10 00:27:59 +0530

    about section finished

--- components/About.jsx
+++ components/About.jsx
@@ -1,8 +1,55 @@
+import { assets, infoList, toolsData } from "@/assets/assets";
+import Image from "next/image";
+
 const About = () => {
   return (
     <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
       <h2 className="text-center mb-2 text-lg font-ovo">Introduction</h2>
       <p className="text-center text-5xl font-ovo">About Me</p>
+
+      <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
+        <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+          <Image
+            src={assets.user_image}
+            alt="Shubham"
+            className="w-full rounded-3xl"
+          />
+        </div>
+        <div className="flex-1">
+          <p className="mb-10 max-w-2xl font-ovo">
+            I am an experienced Full Stack Developer with over 5 years of
+            professional expertise in the field. Throughout my career, I have
+            had the privilage of collaborating with prestigious organizations,
+            contributing to their success and growth.
+          </p>
+
+          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+            {infoList.map(({ id, icon, iconDark, title, description }) => (
+              <li
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-light-hover hover:-translate-y-1 duration-500 hover:shadow-light"
+                key={id}
+              >
+                <Image src={icon} alt={title} className="w-7 mt-3" />
+                <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
+                <p className="text-gray-600 text-sm">{description}</p>
+              </li>
+            ))}
+          </ul>
+
+          <h4 className="my-6 text-gray-700 font-ovo">My Tools</h4>
+
+          <ul className="flex items-center gap-3 sm:gap-5">
+            {toolsData.map(({ id, tool }) => (
+              <li
+                key={id}
+                className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
+              >
+                <Image src={tool} alt="tool" className="w-5 sm:w-7" />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
