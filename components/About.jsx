8342d71646ec9504465c404bc9de60279f262601
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-09 23:57:36 +0530

    about me section updated

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,10 @@
 const About = () => {
-    return (
-        <div>About</div>
-    );
+  return (
+    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h2 className="text-center mb-2 text-lg font-ovo">Introduction</h2>
+      <p className="text-center text-5xl font-ovo">About Me</p>
+    </div>
+  );
 };
 
 export default About;
