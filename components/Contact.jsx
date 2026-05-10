0962a17ba2f56e7c47cb2be1bc20ecee6f8f9cd6
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-11 00:43:37 +0530

    contact section added

---
+++ components/Contact.jsx
@@ -0,0 +1,15 @@
+const Contact = () => {
+  return (
+    <div id="contact" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h2 className="text-center mb-2 text-lg font-ovo">My Portfolio</h2>
+      <p className="text-center text-5xl font-ovo">My Latest Work</p>
+
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+        Welcome to my web development portfolio! Explore a collection of
+        projects showcasing my expertise in full stack web development.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
