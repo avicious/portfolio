913ca47938dcf06fdb92176f4ac1c251393c985c
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-10 23:49:20 +0530

    services section updated

--- components/Services.jsx
+++ components/Services.jsx
@@ -3,6 +3,10 @@ const Services = () => {
     <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
       <h2 className="text-center mb-2 text-lg font-ovo">What I offer</h2>
       <p className="text-center text-5xl font-ovo">My Services</p>
+
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+        I am a full stack web developer with 5 years of work experience.
+      </p>
     </div>
   );
 };
