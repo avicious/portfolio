b9ed518b88d3d110cac73a572bea521bd3c36f4a
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-30 23:58:31 +0530

    footer updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,14 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    );
+  return (
+    <div className="mt-20">
+      <div className="text-center">
+        <Image src={assets.logo} alt="Shubham" className="w-36 mx-auto mb-2" />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
