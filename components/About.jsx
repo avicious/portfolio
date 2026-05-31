7b1adfca18e1572a528a14dac4e989873135d177
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-31 23:59:07 +0530

    about updated for dark mode

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,7 @@
 import { assets, infoList, toolsData } from "@/assets/assets";
 import Image from "next/image";
 
-const About = () => {
+const About = ({ theme }) => {
   return (
     <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
       <h2 className="text-center mb-2 text-lg font-ovo">Introduction</h2>
@@ -26,17 +26,27 @@ const About = () => {
           <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
             {infoList.map(({ id, icon, iconDark, title, description }) => (
               <li
-                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-light-hover hover:-translate-y-1 duration-500 hover:shadow-light"
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-light-hover hover:-translate-y-1 duration-500 hover:shadow-light dark:border-white dark:hover:shadow-white dark:hover:bg-dark-hover/50"
                 key={id}
               >
-                <Image src={icon} alt={title} className="w-7 mt-3" />
-                <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
-                <p className="text-gray-600 text-sm">{description}</p>
+                <Image
+                  src={theme === "dark" ? iconDark : icon}
+                  alt={title}
+                  className="w-7 mt-3"
+                />
+                <h3 className="my-4 font-semibold text-gray-700 dark:text-white">
+                  {title}
+                </h3>
+                <p className="text-gray-600 text-sm dark:text-white/80">
+                  {description}
+                </p>
               </li>
             ))}
           </ul>
 
-          <h4 className="my-6 text-gray-700 font-ovo">My Tools</h4>
+          <h4 className="my-6 text-gray-700 font-ovo dark:text-white">
+            My Tools
+          </h4>
 
           <ul className="flex items-center gap-3 sm:gap-5">
             {toolsData.map(({ id, tool }) => (
