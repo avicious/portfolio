85bc4fc9ca3c5ce7a8af5306d809f5c91314261d
Shubham Kumar <shubham.kr2951@gmail.com>
2026-06-01 00:17:48 +0530

    footer updated for dark mode

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,22 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
 
-const Footer = () => {
+const Footer = ({ theme }) => {
   return (
     <div className="mt-20">
       <div className="text-center">
-        <Image src={assets.logo} alt="Shubham" className="w-36 mx-auto mb-2" />
+        <Image
+          src={theme === "dark" ? assets.logo_dark : assets.logo}
+          alt="Shubham"
+          className="w-36 mx-auto mb-2"
+        />
 
         <div className="w-max flex items-center gap-2 mx-auto">
-          <Image src={assets.mail_icon} alt="mail" className="w-6" />
+          <Image
+            src={theme === "dark" ? assets.mail_icon_dark : assets.mail_icon}
+            alt="mail"
+            className="w-6"
+          />
           shubham.kr2951@gmail.com
         </div>
       </div>
@@ -18,7 +26,7 @@ const Footer = () => {
         <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
           <li>
             <a target="_blank" href="">
-              Github 
+              Github
             </a>
           </li>
           <li>
