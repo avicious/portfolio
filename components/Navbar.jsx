a60e2430364665fef39a1f9b755d81cf052b55a1
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-31 23:47:50 +0530

    navbar updated for dark mode

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = ({ theme, setTheme }) => {
       >
         <a href="#top">
           <Image
-            src={assets.logo}
+            src={theme === "dark" ? assets.logo_dark : assets.logo}
             alt="Shubham"
             className="w-28 cursor-pointer mr-14"
           />
@@ -75,14 +75,24 @@ const Navbar = ({ theme, setTheme }) => {
             className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo dark:border-white/50"
           >
             Contact{" "}
-            <Image src={assets.arrow_icon} alt="arrow" className="w-3" />
+            <Image
+              src={
+                theme === "dark" ? assets.arrow_icon_dark : assets.arrow_icon
+              }
+              alt="arrow"
+              className="w-3"
+            />
           </a>
 
           <button
             className="cursor-pointer block md:hidden ml-3"
             onClick={openMenu}
           >
-            <Image src={assets.menu_black} alt="menu" className="w-6" />
+            <Image
+              src={theme === "dark" ? assets.menu_white : assets.menu_black}
+              alt="menu"
+              className="w-6"
+            />
           </button>
         </div>
 
@@ -90,11 +100,11 @@ const Navbar = ({ theme, setTheme }) => {
 
         <ul
           ref={sideMenuRef}
-          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 font-ovo"
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 font-ovo dark:bg-dark-hover dark:text-white"
         >
           <div className="absolute right-6 top-6" onClick={closeMenu}>
             <Image
-              src={assets.close_black}
+              src={theme === "dark" ? assets.close_white : assets.close_black}
               alt="close"
               className="w-5 cursor-pointer"
             />
