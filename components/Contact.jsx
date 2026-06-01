5dfde215de7f37c9746d8891869ab2b7d0cd084b
Shubham Kumar <shubham.kr2951@gmail.com>
2026-06-02 00:23:55 +0530

    framer motion added to contact section

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,7 @@
-import { assets } from "@/assets/assets";
+import { motion } from "motion/react";
 import toast from "react-hot-toast";
 import Image from "next/image";
+import { assets } from "@/assets/assets";
 
 const ACCESS_KEY = process.env.NEXT_PUBLIC_WEB3FORMS_KEY;
 const FORM_URL = process.env.NEXT_PUBLIC_WEB3FORMS_URL;
@@ -33,28 +34,62 @@ const Contact = () => {
   };
 
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
       id="contact"
       className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-size-[90%_auto] dark:bg-none'
     >
-      <h2 className="text-center mb-2 text-lg font-ovo">Connect with me</h2>
-      <p className="text-center text-5xl font-ovo">Get in touch</p>
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="text-center mb-2 text-lg font-ovo"
+      >
+        Connect with me
+      </motion.h2>
+      <motion.p
+        initial={{ y: 20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="text-center text-5xl font-ovo"
+      >
+        Get in touch
+      </motion.p>
 
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo"
+      >
         I'd love to hear from you! If you have any questions, comments, or
         feedback, please use the form below.
-      </p>
+      </motion.p>
 
-      <form className="max-w-2xl mx-auto" onSubmit={onSubmit}>
+      <motion.form
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.9 }}
+        className="max-w-2xl mx-auto"
+        onSubmit={onSubmit}
+      >
         <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6 mt-10 mb-8">
-          <input
+          <motion.input
+            initial={{ x: -50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.1 }}
             type="text"
             placeholder="Enter your name"
             name="name"
             required
             className="flex-1 px-3 py-2 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-dark-hover/30 dark:border-white/90"
           />
-          <input
+          <motion.input
+            initial={{ x: 50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.1 }}
             type="email"
             placeholder="Enter your email"
             name="email"
@@ -62,25 +97,29 @@ const Contact = () => {
             className="flex-1 px-3 py-2 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-dark-hover/30 dark:border-white/90"
           />
         </div>
-        <textarea
+        <motion.textarea
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1.1 }}
           rows="6"
           placeholder="Enter your message"
           name="message"
           required
           className="w-full px-3 py-2 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-dark-hover/30 dark:border-white/90"
-        ></textarea>
+        ></motion.textarea>
 
-        <button
+        <motion.button
+          whileHover={{ scale: 1.05 }}
           className="py-3 px-12 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 cursor-pointer dark:bg-transparent dark:hover:bg-dark-hover dark:border-[0.5px] dark:border-white/90"
           type="submit"
         >
           Submit Now
           <Image src={assets.right_arrow_white} alt="arrow" className="w-4" />
-        </button>
+        </motion.button>
 
         <p></p>
-      </form>
-    </div>
+      </motion.form>
+    </motion.div>
   );
 };
 
