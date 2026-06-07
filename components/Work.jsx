7a7c8e0e79932c5d435e74f3c15dffa9d1734180
Shubham Kumar <shubham.kr2951@gmail.com>
2026-06-07 23:42:34 +0530

    framer motion animations updated

--- components/Work.jsx
+++ components/Work.jsx
@@ -12,9 +12,7 @@ const Work = ({ theme }) => {
       className="w-full px-[12%] py-10 scroll-mt-20"
     >
       <motion.h2
-        initial={{ y: -20, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.3 }}
+        
         className="text-center mb-2 text-lg font-ovo"
       >
         My Portfolio
