d0dd6b1ce1be059b9eaf2119624e2eef42b59a35
Shubham Kumar <shubham.kr2951@gmail.com>
2026-06-08 22:29:32 +0530

    framer motion animations updated

--- components/Work.jsx
+++ components/Work.jsx
@@ -64,7 +64,9 @@ const Work = ({ theme }) => {
         ))}
       </motion.div>
       <motion.a
-         
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 1.1 }} 
         href=""
         className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-light-hover duration-500 dark:text-white dark:border-white dark:hover:bg-dark-hover"
       >
